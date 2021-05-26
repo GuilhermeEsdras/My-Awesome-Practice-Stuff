3ab542681e6510000327dc3c9afcb1081e94f854
GuilhermeEsdras <guilherme.esdras@academico.ifpb.edu.br>
2021-05-26 15:16:19 -0300

    Day 15

---
+++ HackerRank/30 Days of Code/Day 15/linked-list.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>	
+typedef struct Node
+{
+    int data;
+    struct Node* next;
+}Node;
+
+Node* insert(Node *head,int data)
+{
+    Node* novoNo = (Node*) malloc(sizeof(Node));
+
+    novoNo->data = data;
+    novoNo->next = NULL;
+
+    if (head == NULL) head = novoNo;
+    else {
+      Node* aux = head;
+      while (aux->next != NULL) {
+          aux = aux->next;
+      }
+
+      aux->next = novoNo;
+    }
+    return head;
+}
+
+void display(Node *head)
+{
+	Node *start=head;
+	while(start)
+	{
+		printf("%d ",start->data);
+		start=start->next;
+	}
+}
+int main()
+{
+	int T,data;
+    scanf("%d",&T);
+    Node *head=NULL;
+    while(T-->0){
+        scanf("%d",&data);
+        head=insert(head,data);
+    }
+  display(head);
+		
+}
